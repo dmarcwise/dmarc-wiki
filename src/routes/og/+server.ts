29605e4902980a8a5140224e0a8e823b56af9b4f
Matteo Contrini <matteo@contrini.it>
2024-10-05 22:51:38 +0200

    Add dynamic og image experiment

---
+++ src/routes/og/+server.ts
@@ -0,0 +1,32 @@
+import { ImageResponse } from '@ethercorps/sveltekit-og';
+import type { RequestHandler } from '@sveltejs/kit';
+import OpenGraphImage from './OpenGraphImage.svelte';
+import { promises as fs } from 'fs';
+
+export const GET: RequestHandler = async () => {
+	const zillaFont = await fs.readFile('src/routes/og/zilla-slab-latin-600-normal.ttf');
+	const interFont = await fs.readFile('src/routes/og/inter-latin-500-normal.ttf');
+
+	return new ImageResponse(
+		// @ts-expect-error typings are wrong
+		OpenGraphImage,
+		{
+			debug: true,
+			fonts: [
+				{
+					name: 'Zilla Slab',
+					data: zillaFont,
+					weight: 600,
+					style: 'normal'
+				},
+				{
+					name: 'Inter',
+					data: interFont,
+					weight: 500,
+					style: 'normal'
+				}
+			]
+		},
+		{}
+	);
+};
---
+++ src/routes/og/OpenGraphImage.svelte
@@ -0,0 +1,34 @@
+<div tw="bg-gray-50 flex flex-col w-full h-full py-14 px-14">
+	<img src="http://localhost:5174/src/lib/logo.png" alt="DMARC.wiki"
+			 tw="h-16" />
+
+	<div tw="mt-auto flex items-center">
+		<div tw="flex w-28 h-28 rounded-xl border border-slate-200 p-2 relative top-3">
+			<img src="http://localhost:5174/src/lib/logos/google.png" alt="Logo"
+					 tw="w-full rounded-lg" />
+		</div>
+
+		<p tw="ml-7 mt-8" style="font-size: 90px">
+			Google Workspace
+		</p>
+	</div>
+
+	<div tw="flex mt-8">
+		<div tw="rounded-lg border px-4 py-2 font-medium flex items-center
+				   bg-green-100 border-green-200 text-green-700 text-3xl"
+				 style="font-family: 'Inter'">
+			SPF alignment
+			<img src="http://localhost:5174/src/lib/emojis/check.png" alt="Yes"
+					 tw="w-8 h-8 ml-3" />
+		</div>
+
+		<div tw="ml-3 rounded-lg border px-4 py-2 font-medium flex items-center
+				   bg-green-100 border-green-200 text-green-700 text-3xl"
+				 style="font-family: 'Inter'">
+			DKIM alignment
+			<img src="http://localhost:5174/src/lib/emojis/check.png" alt="Yes"
+					 tw="w-8 h-8 ml-3" />
+		</div>
+	</div>
+
+</div>
