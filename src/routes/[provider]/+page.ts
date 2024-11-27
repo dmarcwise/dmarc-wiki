1af6f9e6f6ccae4e647222e2bad26e56059c6c8d
Matteo Contrini <matteo@contrini.it>
2024-11-27 14:34:07 +0100

    Add og image meta tags

--- src/routes/[provider]/+page.ts
+++ src/routes/[provider]/+page.ts
@@ -11,17 +11,21 @@ export async function load({ params, parent }) {
 		error(404);
 	}
 
+	const ogImage = `https://dmarc.wiki/${provider.slug}.png`;
+
 	return {
 		provider,
 		seo: {
 			title: `How to set up SPF, DKIM and DMARC for ${provider.name}`,
 			description: `Learn how to set up SPF, DKIM and DMARC for your custom domain on ${provider.name}.`,
 			ogType: 'article',
+			ogImage: ogImage,
 			jsonLd: {
 				'@context': 'https://schema.org',
 				'@type': 'NewsArticle',
 				headline: `How to set up SPF, DKIM and DMARC for ${provider.name}`,
-				dateModified: provider.updated.toISOString()
+				dateModified: provider.updated.toISOString(),
+				image: ogImage
 			}
 		}
 	};
